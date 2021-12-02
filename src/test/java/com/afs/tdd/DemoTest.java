2d7c6ef93806a1c3c92b464f987bdd78140a8f47
Gloria Chan <changl3@oocl.com>
2021-12-02 17:04:15 +0800

    [java-tdd-mars-rover][Gloria] - feat: testcase 9 init commit

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -100,4 +100,16 @@ class MarsRoverTest {
         //then
         assertEquals("0 0 N", actual);
     }
+
+    @Test
+    void should_return_locationY_add1_when_moveForward_given_locationPoint_W() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        String command = "M";
+        //when
+        marsRover.executeCommand(command);
+        String actual = marsRover.getStatus();
+        //then
+        assertEquals("0 1 N", actual);
+    }
 }
