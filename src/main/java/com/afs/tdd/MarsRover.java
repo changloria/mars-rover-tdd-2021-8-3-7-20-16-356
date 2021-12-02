2b2cbe9aab62940db0656d5207932953ef073688
Gloria Chan <changl3@oocl.com>
2021-12-02 16:24:50 +0800

    [java-TDD-MarsRover][Gloria] -feat: testcase 1 init commit

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,15 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    public MarsRover(){
+
+    }
+
+    public MarsRover(int locationX, int locationY, String direction) {
+
+    }
+
+    public String executeCommand(String command) {
+        return null;
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,18 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_return_locationPoint_W_when_turnLeft_given_locationPoint_N() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        String command = "L";
+        //when
 
+        String actual = marsRover.executeCommand(command);
+        //then
+        assertEquals("0 0 W", actual);
     }
 }
